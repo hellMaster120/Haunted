16916f499d951a748cb189d7af785173ab3d53cf
mc-kshellenbarger24 <114780474+mc-kshellenbarger24@users.noreply.github.com>
2022-10-18 10:36:10 -0400

    Update Index.js

--- JS/Index.js
+++ JS/Index.js
@@ -4,21 +4,31 @@ var LoginButton = document.getElementsByClassName("g_id_signin")[0];
 var TestingMode = true
 
 if(TestingMode == true){
-
     LoginButton.style.top = "0px";
     LoginButton.style.right = "20px";
     Background.style.filter = "blur(0px)";
+    localStorage.setItem("Login", JSON.stringify({"Data":"Dev","Logined":true}));
 }
 
 function handleCredentialResponse(Data){
     LoginButton.style.top = "0px";
     LoginButton.style.right = "20px";
     Background.style.filter = "blur(0px)";
+    localStorage.setItem("Login", {"Data":Data,"Logined":true});
 }
 
 Background.addEventListener("click",(Data)=>{
     console.log("YOUR MOM")
-    if(Data.target == document.getElementById("Door")){
+    var StorageData = JSON.parse(localStorage.getItem("Login"))
+    if(Data.target == document.getElementById("Door") && StorageData["Logined"]){
         window.location = "../HTML/index1.html" 
     }
+   
 })
+
+window.onunload(ClearData)
+window.onbeforeunload(ClearData)
+
+function ClearData(){
+    localStorage.clear()
+}
