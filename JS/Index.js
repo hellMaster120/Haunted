e7da2768d112ef3014d69fd4c4dc231a77a78889
mc-kshellenbarger24 <114780474+mc-kshellenbarger24@users.noreply.github.com>
2022-10-18 09:18:33 -0400

    Update Index.js

--- JS/Index.js
+++ JS/Index.js
@@ -1,18 +1,24 @@
+
+var Background = document.getElementById("Background");
+var LoginButton = document.getElementsByClassName("g_id_signin")[0];
 var TestingMode = true
 
 if(TestingMode == true){
-    var Background = document.getElementById("Background");
-    var LoginButton = document.getElementsByClassName("g_id_signin")[0];
+
     LoginButton.style.top = "0px";
     LoginButton.style.right = "20px";
     Background.style.filter = "blur(0px)";
 }
 
 function handleCredentialResponse(Data){
-    console.log(Data)
-    var Background = document.getElementById("Background");
-    var LoginButton = document.getElementsByClassName("g_id_signin")[0];
     LoginButton.style.top = "0px";
     LoginButton.style.right = "20px";
     Background.style.filter = "blur(0px)";
 }
+
+Background.addEventListener("click",(Data)=>{
+    console.log("YOUR MOM")
+    if(Data.target == document.getElementById("Door")){
+        window.location = "../HTML/index1.html" 
+    }
+})
